02d794c21810febcc9589051f5f58a098d484ebd
Tarlan Huseynov <tarlan.huseynov@azintelecom.az>
2024-07-29 14:49:57 +0400

    init

--- backend.sh
+++ backend.sh
@@ -1,51 +1,51 @@
-#!/bin/bash
+#!/bin/sh
 set -euo pipefail
 
-PROJECT_NAME=$(basename "$(dirname \"${PWD}\")")
+PROJECT_NAME=terraform-ansible-ssm
 AWS_REGION=${AWS_REGION:-us-east-2}
 AWS_PROFILE=${AWS_PROFILE:-default}
-AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text --profile ${AWS_PROFILE})"
+AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
 export AWS_PAGER=""
 
-echo -e "Bootstraping terraform backend...\n"
+
+
+echo -e "Bootstrapping terraform backend...\n"
 echo PROJECT_NAME: "${PROJECT_NAME}"
 echo AWS_REGION: "${AWS_REGION}"
 echo AWS_PROFILE: "${AWS_PROFILE}"
 echo AWS_ACCOUNT_ID: "${AWS_ACCOUNT_ID}"
-echo BUCKET NAME: "terraform-tfstate-${PROJECT_NAME}"
-echo DYNAMODB TABLE NAME: terraform-locks
+echo BUCKET NAME: "tfstate-${PROJECT_NAME}-${RANDOM_STRING}"
+echo DYNAMODB TABLE NAME: "${PROJECT_NAME}-terraform-locks"
 echo -e "\n"
 
 aws s3api create-bucket \
 	--region "${AWS_REGION}" \
 	--create-bucket-configuration LocationConstraint="${AWS_REGION}" \
-	--bucket "terraform-tfstate-${PROJECT_NAME}" \
-	--profile "${AWS_PROFILE}"
+	--bucket "tfstate-${PROJECT_NAME}-${RANDOM_STRING}" 
 
 aws dynamodb create-table \
 	--region "${AWS_REGION}" \
-	--table-name terraform-locks \
+	--table-name ${PROJECT_NAME}-terraform-locks \
 	--attribute-definitions AttributeName=LockID,AttributeType=S \
 	--key-schema AttributeName=LockID,KeyType=HASH \
-	--provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
-	--profile "${AWS_PROFILE}"
+	--provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
 
 cat <<EOF > ./backend.tf
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.57"
+      version = "~> 5.60"
     }
   }
 
   required_version = ">=1.9.0"
 
   backend "s3" {
-    bucket         = "terraform-tfstate-${PROJECT_NAME}"
+    bucket         = "tfstate-${PROJECT_NAME}-${RANDOM_STRING}"
     key            = "${PROJECT_NAME}"
     region         = "${AWS_REGION}"
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "${PROJECT_NAME}-terraform-locks"
   }
 }
 
--- backend.tf
+++ backend.tf
@@ -2,17 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.57"
+      version = "~> 5.60"
     }
   }
 
   required_version = ">=1.9.0"
 
   backend "s3" {
-    bucket         = "terraform-tfstate-k8s-downscaler"
-    key            = "k8s-downscaler"
+    bucket         = "tfstate-terraform-ansible-ssm-demo"
+    key            = "terraform-ansible-ssm"
     region         = "us-east-2"
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "terraform-ansible-ssm-terraform-locks"
   }
 }
 
